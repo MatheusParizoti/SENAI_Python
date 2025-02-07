e580385254d4c877cc9da32019d3a3b4fa2c52e9
Matheus Parizoti <134006837+MatheusParizoti@users.noreply.github.com>
2025-02-07 12:48:55 -0300

    Delete aula5 directory

--- aula5/app.py
+++
@@ -1,9 +0,0 @@
-n1 = input("Digite o número de pés: ")
-
-polegadas = int(n1) * 12
-jardas = int(n1) / 3
-milhas = jardas / 1760
-
-print("Polegadas: ", polegadas)
-print("Jardas: ", jardas)
-print("Milhas: ", milhas)
